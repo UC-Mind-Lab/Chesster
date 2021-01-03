0ebdda2a1d05ec75404fd23ca5d987c4e3a2535b
Tyler Westland <tylerofthewest@gmail.com>
2021-01-03 14:47:48 -0600

    Adds highlighting of last move.
    Now highlights the previous move.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    version="0.26.0",
+    version="0.27.0",
     setup_requires=[],
     test_require=[],
     include_package_data=True,

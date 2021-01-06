27c37e531e596ed38e850b4e69d5530ba0a48e01
Tyler Westland <tylerofthewest@gmail.com>
2021-01-06 08:17:38 -0600

    Saves the random move making to a separate method.
    Might be useful later

--- src/chesster/ai/tyler_westland.py
+++ src/chesster/ai/tyler_westland.py
@@ -12,6 +12,23 @@ class TylerWestlandAI(BaseAI):
     Pick a random move.
 
     """
+    @staticmethod
+    def make_random_legal_move(board:chess.Board) -> chess.Move:
+        """Return a random legal move.
+
+        Parameters
+        ----------
+        board: chess.Board
+            The chessboard to analyze and make a move upon.
+
+        Returns
+        -------
+        chess.Move
+            Move to make.
+        """
+        return random.choice(list(board.legal_moves))
+
+
     def make_move(self, board:chess.Board, timer:BaseTimer) -> chess.Move:
         """Return a really well thought out move.
 
@@ -27,5 +44,5 @@ class TylerWestlandAI(BaseAI):
         chess.Move
             Move to make.
         """
-        return random.choice(list(board.legal_moves))
+        return self.make_random_legal_move(board)
 
